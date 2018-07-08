f6e9cf546463d1e05598a86386d70e7e639ef8fc
Roger DeMagri <rjpd@netzero.com>
2018-07-08 13:46:57 -0400

    Close & menu

--- WFA1/Form1.cs
+++ WFA1/Form1.cs
@@ -16,5 +16,15 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
